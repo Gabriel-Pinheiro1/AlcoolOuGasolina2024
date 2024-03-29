0181c5fbb5c34581fdc1e3d3fc6fdda5bba0edf3
Gabriel Pinheiro <103785962+Gabriel-Pinheiro1@users.noreply.github.com>
2024-03-29 18:15:32 -0300

    .

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.kt
@@ -7,24 +7,61 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Switch
+
 
 class MainActivity : AppCompatActivity() {
     var percentual:Double = 0.7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("PDM24.1","No onCreate, $percentual")
+        if (savedInstanceState != null) {
+            percentual=savedInstanceState.getDouble("percentual")
+        }
+        Log.d("PDM24", "No onCreate, $percentual")
 
         percentual=0.7
         val btCalc: Button = findViewById(R.id.btCalcular)
-        val textMsg:TextView= findViewById(R.id.textMsg)
+
+        val switchPercentual: Switch = findViewById(R.id.swPercentual)
+
+        switchPercentual.setOnCheckedChangeListener { _, isChecked ->
+            percentual = if (isChecked) {
+                0.75
+            } else {
+                0.7
+            }
+
+            Log.d("PDM24", "Percentual alterado para $percentual")
+        }
+
         //btCalc.setBackgroundColor(Color.CYAN)
         btCalc.setOnClickListener(View.OnClickListener {
             //código do evento
-            percentual=0.75
-            textMsg.text="Já tenho o percentual"
+            escolherCombustivel(percentual)
+
             Log.d("PDM24","No btCalcular, $percentual")
         })
+    }
+
+    fun escolherCombustivel(percentual: Double){
+        val txtGasolina: TextView = findViewById(R.id.edGasolina)
+        val txtAlcool: TextView = findViewById(R.id.edAlcool)
+        val resultado: TextView = findViewById(R.id.textMsg)
+
+        if (txtAlcool.text.toString() != "" && txtGasolina.text.toString() != "") {
+            val gasolina: Double = txtGasolina.text.toString().toDouble()
+            val alcool: Double = txtAlcool.text.toString().toDouble()
+
+
+            resultado.text = if (alcool <= gasolina * percentual){
+                "É mais vantajoso usar álcool"
+            } else{
+                "É mais vantajoso usar gasolina"
+            }
+        } else {
+            resultado.text = "Por favor, preencha todos os campois acima"
+        }
     }
 override fun onResume(){
     super.onResume()
@@ -46,4 +83,8 @@ override fun onDestroy(){
     super.onDestroy()
     Log.wtf("PDM24","No Destroy")
 }
+ override fun onSaveInstanceState(outState: Bundle) {
+    outState.putDouble("percentual",percentual)
+    super.onSaveInstanceState(outState)
+    }
 }
